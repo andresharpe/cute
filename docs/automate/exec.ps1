0d05862a483cb6c26a0e5d1ecd7429bb55efb1d0
andre <andre.sharpe@gmail.com>
2024-07-13 15:18:13 +0200

    Updated auto generated readme images

--- docs/automate/exec.ps1
+++ docs/automate/exec.ps1
@@ -1,3 +1,5 @@
 . .\docs\automate\Get-Screen.ps1
+[Console]::CursorVisible = $false
 cls && dotnet run --project .\source\Cute\Cute.csproj -- -help && Get-Screen .\docs\images\help.png -CropBottom 550
 cls && dotnet run --project .\source\Cute\Cute.csproj -- login -help && Get-Screen .\docs\images\login.png -CropBottom 850
+[Console]::CursorVisible = $true
