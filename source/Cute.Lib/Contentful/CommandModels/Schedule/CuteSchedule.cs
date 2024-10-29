2e11bf4ed886c653a64a4614fe703ad47cfc2fde
tengo-lebanidze <tengo.lebanidze@gmail.com>
2024-10-29 12:43:55 +0400

    Revamp cute scheduling

---
+++ source/Cute.Lib/Contentful/CommandModels/Schedule/CuteSchedule.cs
@@ -0,0 +1,27 @@
+﻿using Contentful.Core.Models;
+
+namespace Cute.Lib.Contentful.CommandModels.Schedule
+{
+    public class CuteSchedule : IContent
+    {
+        public SystemProperties Sys { get; set; } = default!;
+        public string Id => Sys != null ? Sys.Id : "";
+        public string Key { get; set; } = default!;
+        public string Command { get; set; } = default!;
+        public string Schedule { get; set; } = default!;
+
+        public string? CronSchedule { get; set; } = default!;
+
+        public CuteSchedule? RunAfter { get; set; } = default!;
+
+        public string? LastRunStatus { get; set; }
+
+        public string? LastRunErrorMessage { get; set; }
+
+        public DateTime? LastRunStarted { get; set; }
+        public DateTime? LastRunFinished { get; set; }
+        public string? LastRunDuration { get; set; }
+        public bool IsRunAfter => RunAfter != null;
+        public bool IsTimeScheduled => !IsRunAfter;
+    }
+}
---
+++ source/Cute.Lib/Contentful/CommandModels/Schedule/CuteScheduleContentType.cs
@@ -0,0 +1,64 @@
+﻿using Contentful.Core.Models;
+using Contentful.Core.Search;
+using Cute.Lib.Enums;
+
+namespace Cute.Lib.Contentful.CommandModels.Schedule
+{
+    public class CuteScheduleContentType
+    {
+        private static readonly ContentType _contentType;
+
+        public static ContentType Instance()
+        {
+            return _contentType;
+        }
+
+        static CuteScheduleContentType()
+        {
+            var contentTypeBuilder = new ContentTypeBuilder(nameof(CuteSchedule).ToCamelCase())
+                .WithDescription("Jobs and definitions for synchronising the space with external API's.")
+                .WithDisplayField("key")
+                .WithFields([
+
+                    new FieldBuilder("key", FieldType.Symbol)
+                    .IsRequired()
+                    .IsUnique()
+                    .Build(),
+
+                    new FieldBuilder("command", FieldType.Text)
+                        .IsRequired()
+                        .Build(),
+
+                    new FieldBuilder("schedule", FieldType.Symbol)
+                        .IsRequired()
+                        .DefaultValue("en", "never")
+                        .Build(),
+
+                    new FieldBuilder("cronSchedule", FieldType.Symbol)
+                        .Build(),
+
+                    new FieldBuilder("runAfter", FieldType.Link)
+                        .ValidateLinkContentType([nameof(CuteSchedule).ToCamelCase()], LinkType.Entry)
+                        .Build(),
+
+                    new FieldBuilder("lastRunStatus", FieldType.Symbol)
+                        .Build(),
+
+                    new FieldBuilder("lastRunErrorMessage", FieldType.Text)
+                        .Build(),
+
+                    new FieldBuilder("lastRunStarted", FieldType.Date)
+                        .Build(),
+
+                    new FieldBuilder("lastRunFinished", FieldType.Date)
+                        .Build(),
+
+                    new FieldBuilder("lastRunDuration", FieldType.Symbol)
+                        .Build(),
+
+                ]);
+
+            _contentType = contentTypeBuilder.Build();
+        }
+    }
+}
