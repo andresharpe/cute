1ec88f1e7886db7a77f6e00315032770cd3eba49
tengo-lebanidze <tengo.lebanidze@gmail.com>
2025-09-16 11:12:15 +0400

    Fix glossary type and add to test data command

--- source/Cute.Lib/Contentful/CommandModels/TranslationGlossary/CuteTranslationGlossary.cs
+++ source/Cute.Lib/Contentful/CommandModels/TranslationGlossary/CuteTranslationGlossary.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cute.Lib.Contentful.CommandModels.TranslationGlossary
+﻿namespace Cute.Lib.Contentful.CommandModels.TranslationGlossary
 {
     public class CuteTranslationGlossary
     {
